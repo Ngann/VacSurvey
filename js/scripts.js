92828451e8c7836dbbaca5d8d9e89dd74a21c3d8
Ngann <ngancamnguyen@gmail.com>
2018-02-25 17:54:56 -0800

    Add Next button to each question in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 
+/*
   $(".button").click(function() {
     $("#destination1").toggle();
-  /*  alert("I told you, THIS IS A HEADER!");  */
+   alert("I told you, THIS IS A HEADER!");  */
+
+    $("button").submit(function(event) {
+     event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      var result = add(number1, number2);
+      $("#output").text(result);
+    });
   });
+
 
 });
