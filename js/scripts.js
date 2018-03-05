b1b02c07ff171643a018f8b30bd285ac7c30c9e5
Ngann <ngancamnguyen@gmail.com>
2018-03-04 23:24:25 -0800

    added varibles

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,99 @@
+var choice_a = 0;
+var choice_b = 0;
+var choice_c = 0;
+
 $(document).ready(function() {
 
   $(".ButtonOne").click(function(event) {
+    event.preventDefault();
     $("#Q1").hide();
     $("#Q2").show();
-    event.preventDefault();
+
+    var input = $('#Q1 input:checked').val();
+    if (input === 'A') {
+      choice_a += 1;
+    } else if (input === 'B') {
+      choice_b += 1;
+    } else if (input === 'C') {
+      choice_c += 1;
+    }
+    console.log('a', choice_a, 'b', choice_b, 'c', choice_c);
   });
 
   $(".ButtonTwo").click(function(event){
+    event.preventDefault();
     $("#Q2").hide();
     $("#Q3").show();
-    event.preventDefault();
+
+    var input = $('#Q2 input:checked').val();
+    if (input === 'A') {
+      choice_a += 1;
+    } else if (input === 'B') {
+      choice_b += 1;
+    } else if (input === 'C') {
+      choice_c += 1;
+    }
+    console.log('a', choice_a, 'b', choice_b, 'c', choice_c);
   });
 
   $(".ButtonThree").click(function(event){
+    event.preventDefault();
     $("#Q3").hide();
     $("#Q4").show();
-    event.preventDefault();
+
+    var input = $('#Q3 input:checked').val();
+    if (input === 'A') {
+      choice_a += 1;
+    } else if (input === 'B') {
+      choice_b += 1;
+    } else if (input === 'C') {
+      choice_c += 1;
+    }
+    console.log('a', choice_a, 'b', choice_b, 'c', choice_c);
   });
 
   $(".ButtonFour").click(function(event){
+    event.preventDefault();
     $("#Q4").hide();
     $("#Q5").show();
-    event.preventDefault();
+
+    var input = $('#Q4 input:checked').val();
+    if (input === 'A') {
+      choice_a += 1;
+    } else if (input === 'B') {
+      choice_b += 1;
+    } else if (input === 'C') {
+      choice_c += 1;
+    }
+    console.log('a', choice_a, 'b', choice_b, 'c', choice_c);
   });
 
   $(".ButtonFive").click(function(event){
+    event.preventDefault();
     $("#Q5").hide();
     $(".info").show();
-    event.preventDefault();
+
+    var input = $('#Q5 input:checked').val();
+    if (input === 'A') {
+      choice_a += 1;
+    } else if (input === 'B') {
+      choice_b += 1;
+    } else if (input === 'C') {
+      choice_c += 1;
+    }
+    console.log('a', choice_a, 'b', choice_b, 'c', choice_c);
   });
 
   $(".ButtonFinal").click(function (event){
+    event.preventDefault();
+
+
     $(".info").hide();
     $(".result").show();
     $("span#firstname").text($("input#firstname").val())
     $("span#lastname").text($("input#lastname").val())
     $("span#place").text($("input#place").val())
     $("span#packing").text($("input#packing").val())
-    event.preventDefault();
   });
 
 /*
